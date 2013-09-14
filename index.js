af5aff92fa467a48fad3e138dbb14eb5354eb8c7
E.Azer Koçulu <azer@kodfabrik.com>
2013-09-14 01:15:14 -0700

    return callback

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ module.exports.off = off;
 
 function on (element, event, callback, capture) {
   (element.addEventListener || element.attachEvent).call(element, event, callback, capture);
+  return callback;
 }
 
 function off (element, event, callback, capture) {
   (element.removeEventListener || element.detachEvent).call(element, event, callback, capture);
+  return callback;
 }
