da1a6c1fa417daad9d397de69446533b876d98b3
Shahar Or <mightyiampresence@gmail.com>
2015-03-30 12:32:55 +0300

    Fix for PhantomJS

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ module.exports.off = off;
 
 function on (element, event, callback, capture) {
   !element.addEventListener && (event = 'on' + event);
-  (element.addEventListener || element.attachEvent)(event, callback, capture);
+  (element.addEventListener || element.attachEvent).call(element, event, callback, capture);
   return callback;
 }
 
 function off (element, event, callback, capture) {
   !element.removeEventListener && (event = 'on' + event);
-  (element.removeEventListener || element.detachEvent)(event, callback, capture);
+  (element.removeEventListener || element.detachEvent).call(element, event, callback, capture);
   return callback;
 }
